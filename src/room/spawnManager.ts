1bdf6d9c8238ea54219457eb069e530a303a9efa
Bango88 <47485594+Bango88@users.noreply.github.com>
2022-02-09 12:25:40 -0800

    look for construction

--- src/room/spawnManager.ts
+++ src/room/spawnManager.ts
@@ -1,12 +1,15 @@
+import { generalFuncs } from "Global/globalFuncs";
 
 export function spawnManager() {
   let newName;
+
   let harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
   let haulers = _.filter(Game.creeps, (creep) => creep.memory.role == 'hauler');
   let upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
   let builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
   let repairers = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
-  if (harvesters.length < 6) {
+  let constructionSites = generalFuncs.room?.find(FIND_CONSTRUCTION_SITES);
+  if (harvesters.length < 4) {
     newName = 'Harvester' + Game.time;
     Game.spawns['Spawn1'].spawnCreep([WORK, WORK, MOVE], newName, {
       memory: {role: 'harvester'}
@@ -21,12 +24,12 @@ export function spawnManager() {
     Game.spawns['Spawn1'].spawnCreep([WORK, MOVE, CARRY, MOVE], newName , {
       memory: {role: 'upgrader', upgrading: false}
     });
-  } else if (builders.length < 5) {
+  } else if (constructionSites?.length && builders.length < 4) {
     newName = 'Builder' + Game.time;
     Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, CARRY, MOVE], newName, {
       memory: {role: 'builder', building: false}
     });
-  } else if (repairers.length < 6) {
+  } else if (constructionSites?.length && repairers.length < 4) {
     newName = 'Repairer' + Game.time;
     Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, CARRY, MOVE], newName, {
       memory: {role: 'repairer', repairing: false}
